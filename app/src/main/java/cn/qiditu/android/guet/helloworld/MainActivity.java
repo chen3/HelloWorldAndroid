596151a9a239f6a858c25971c617c144a71d55a9
chen3 <chen3@qiditu.cn>
2017-02-20 11:57:33 +0800

    first commit

---
+++ app/src/main/java/cn/qiditu/android/guet/helloworld/MainActivity.java
@@ -0,0 +1,13 @@
+package cn.qiditu.android.guet.helloworld;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
